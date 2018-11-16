76414861f36785de184f5bbd0f59dbd8c8b3644d
Magnus Feuer <mfeuer1@jaguarlandrover.com>
2018-11-16 15:10:26 -0800

    Fixed patching of PIDs in test data, allowing us to send out-of-order packets. Fixed timeout management to pace sending of packets to subscriber

--- rmc_proto_test_pub.c
+++ rmc_proto_test_pub.c
@@ -14,32 +14,41 @@ static uint8_t _test_print_pending(pub_packet_node_t* node, void* dt)
 
     printf("%*cPacket          %p\n", indent*2, ' ', pack);
     printf("%*c  PID             %lu\n", indent*2, ' ', pack->pid);
-    printf("%*c  Sent timestamp  %lu\n", indent*2, ' ', pack->send_ts);
+    printf("%*c  Sent timestamp  %ld\n", indent*2, ' ', pack->send_ts);
     printf("%*c  Reference count %d\n", indent*2, ' ', pack->ref_count);
     printf("%*c  Parent node     %p\n", indent*2, ' ', pack->parent_node);
     printf("%*c  Payload Length  %d\n", indent*2, ' ', pack->payload_len);
+    printf("%*c  Payload         %s\n", indent*2, ' ', (char*) pack->payload);
     putchar('\n');
     return 1;
 }
 
-void queue_test_data(rmc_context_t* ctx, rmc_test_data_t* td_arr, int td_arr_ind)
+void queue_test_data(rmc_context_t* ctx, rmc_test_data_t* td)
 {
     pub_packet_node *node = 0;
     int res = 0;
-    res = rmc_queue_packet(ctx, td_arr[td_arr_ind].payload, strlen(td_arr[td_arr_ind].payload)+1);
+    res = rmc_queue_packet(ctx, td->payload, strlen(td->payload)+1);
 
     if (res) {
         printf("queue_test_data(payload[%s], pid[%lu]): %s",
-               td_arr[td_arr_ind].payload,
-               td_arr[td_arr_ind].pid,
+               td->payload,
+               td->pid,
                strerror(res));
         exit(255);
     }
 
     // Patch node with the correct pid.
-    pub_packet_list_tail(&ctx->pub_ctx.queued)->data->pid = td_arr[td_arr_ind].pid;
-    pub_packet_list_for_each(&ctx->pub_ctx.queued, _test_print_pending, (void*) (uint64_t) 1);
-    
+    // Find the correct payload and update its pid
+    pub_packet_list_for_each(&ctx->pub_ctx.queued,
+                             lambda (uint8_t, (pub_packet_node_t* node, void* dt) {
+                                     pub_packet_t *pack = node->data;
+                                     if (pack->payload_len == strlen(td->payload) + 1 &&
+                                         !memcmp(pack->payload, td->payload, pack->payload_len)) {
+                                         pack->pid = td->pid;
+                                             return 0;
+                                     }
+                                     return 1;
+                                 }), 0);
 }
 
 
@@ -61,12 +70,14 @@ void test_rmc_proto_pub(char* mcast_group_addr,
     user_data_t ud = { .u64 = 0 };
     int ind = 0;
     int countdown = 10;
-    static rmc_test_data_t td[] = {
-        { "ping", 1, 100 },
+    usec_timestamp_t t_out = 0;
+    static rmc_test_data_t td[] = { 
+        // Wait for 0.01 seconds for return tcp connection coming back in from subscriber
+        { "ping", 1, 100000 }, 
         { "p1", 2, 0 },
         { "p3", 4, 0 },
         { "p2", 3, 0 },
-        { "exit", 5, 1000 }, // Wait for 1000 msec until exiting to process residual event.
+        { "p4", 5, 1000000 }, // Wait for 1 sec until exiting to process residual event.
     };
 
 
@@ -96,22 +107,33 @@ void test_rmc_proto_pub(char* mcast_group_addr,
     printf("rmc_proto_test_pub: context: ctx[%.9X]\n", rmc_context_id(ctx));
 
     for(ind = 0; ind < sizeof(td) / sizeof(td[0]); ) {
-        usec_timestamp_t now = rmc_usec_monotonic_timestamp();
-        usec_timestamp_t wait_until = now + td[ind].msec_wait * 1000;
-        usec_timestamp_t t_out = 0;
+        usec_timestamp_t now =  rmc_usec_monotonic_timestamp();
+        usec_timestamp_t tout =  now + td[ind].msec_wait;
         int tick_ind = 0;
-        queue_test_data(ctx, td, ind);
-        
-        // Process events until it is time to queue and send the next
-        // frame (or quit).
-        while(now <= wait_until) {
-            t_out = (wait_until - now);
-            process_events(ctx, epollfd, t_out, 2, &tick_ind);
+
+        queue_test_data(ctx, &td[ind]);
+
+        // Make sure we run the loop at least one.
+        if (now > tout)
+            now = tout;
+
+        tick_ind = 0;
+        while(now < tout) {
+            usec_timestamp_t event_tout = 0;
+
+            rmc_get_next_timeout(ctx, &event_tout);
+
+            if (event_tout == -1 || event_tout > tout - now)
+                event_tout = tout - now;
+            
+            if ((res = process_events(ctx, epollfd, event_tout, 2, &tick_ind)) == ETIME) 
+                rmc_process_timeout(ctx);
+
             now = rmc_usec_monotonic_timestamp();
-            if (tick_ind)
-                ++ind;
-        }
+        }        
+        ++ind;
+        
     }
+
+    puts("Done");
 }
-
-
