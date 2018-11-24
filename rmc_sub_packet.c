46136a162a1e4c78786e2d4b3e45488170dd655e
Magnus Feuer <mfeuer1@jaguarlandrover.com>
2018-11-24 11:59:59 -0800

    Prep subscriber-calling code for simplified subscriber management.

--- rmc_sub_packet.c
+++ rmc_sub_packet.c
@@ -17,24 +17,45 @@
 
 int rmc_sub_get_dispatch_ready_count(rmc_sub_context_t* ctx)
 {
-    return sub_get_dispatch_ready_count(&ctx->sub_ctx);
+    if (!ctx)
+        return 0;
+
+    return sub_packet_list_size(&ctx->dispatch_ready);
 }
 
 sub_packet_t* rmc_sub_get_next_dispatch_ready(rmc_sub_context_t* ctx)
 {
-    return sub_get_next_dispatch_ready(&ctx->sub_ctx);
+    if (!ctx)
+        return 0;
+
+    if (rmc_sub_get_dispatch_ready_count(ctx))
+        return sub_packet_list_head(&ctx->dispatch_ready)->data;
+
+    return 0;
 }
 
 
+// Caller still need to free pack->payload
 int rmc_sub_packet_dispatched(rmc_sub_context_t* ctx, sub_packet_t* pack)
 {
-    rmc_connection_t* conn = sub_packet_user_data(pack).ptr;
-    uint16_t old_action = 0;
+    sub_packet_node_t* node = 0;
 
-    if (!conn)
+    if (!ctx || !pack)
         return EINVAL;
-    
-    sub_packet_dispatched(pack);
+
+        
+    node = sub_packet_list_find_node_rev(&ctx->dispatch_ready,
+                                         pack,
+                                         lambda(int, (sub_packet_t* a, sub_packet_t* b) {
+                                                 return a == b;
+                                             }));
+
+    if (!node)
+        return ENOENT;
+
+
+    sub_packet_list_delete(node);
+    return 0;
 }
 
 
@@ -57,12 +78,41 @@ int _rmc_sub_single_packet_acknowledged(rmc_sub_context_t* ctx, sub_packet_t* pa
     if (!pack->skip_acknowledgement)
         _rmc_sub_write_single_acknowledgement(ctx, conn, pack->pid);
 
-    sub_packet_acknowledged(pack);
+//    sub_packet_acknowledged(pack);
 
     if (ctx->payload_free)
         (*ctx->payload_free)(pack->payload, pack->payload_len, ctx->user_data);
     else
         free(pack->payload);
+    
+    return 0;
+}
+
+
+int _rmc_sub_packet_interval_acknowledged(rmc_sub_context_t* ctx, sub_pid_interval_t interval)
+{
+    rmc_connection_t* conn = 0;
+
+    if (!ctx)
+        return EINVAL;
+    
+//    conn = sub_packet_user_data(pack).ptr;
+    
+
+//    if (conn->mode != RMC_CONNECTION_MODE_SUBSCRIBER)
+//        return EINVAL;
+
+
+    // If we are to write an ack, then do so.
+//    if (!pack->skip_acknowledgement)
+//        _rmc_sub_write_single_acknowledgement(ctx, conn, pack->pid);
+
+//    sub_packet_acknowledged(pack);
+
+//    if (ctx->payload_free)
+//        (*ctx->payload_free)(pack->payload, pack->payload_len, ctx->user_data);
+//    else
+//        free(pack->payload);
     
     return 0;
 }
